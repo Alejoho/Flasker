5cf7e6b29e47b7bc6a9f52c7dd1b75bdf6579d78
Alejoho <alejoreilly96@gmail.com>
2024-11-15 17:52:15 -0500

    In the middle of the 3rd video

--- app.py
+++ app.py
@@ -26,3 +26,13 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", user_name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 405
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
