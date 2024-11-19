187e582ea0339013f5b7f66adef546a3d6237cfd
Alejoho <alejoreilly96@gmail.com>
2024-11-18 20:31:45 -0500

    End of the 8th video. Using SQLite with flask-sqlalchemy

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SECRET_KEY"] = "my super secret key that no one is supposed to know"
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self) -> str:
+        return f"<Name {self.name}>"
+
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField(
+        "Email",
+        validators=[DataRequired("You need to enter your email")],
+    )
+    submit = SubmitField("Submit")
 
 
 class NamerForm(FlaskForm):
@@ -21,6 +45,29 @@ class NamerForm(FlaskForm):
 # title
 # trim
 # striptags
+
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    my_name = None
+    form = UserForm()
+
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+
+        my_name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User added succesfully!")
+
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template(
+        "add_user.html", form=form, name=my_name, our_users=our_users
+    )
 
 
 @app.route("/")
