01f17e295d67f1bf008e8e3c0fd5fdb402937cf6
Alejoho <alejoreilly96@gmail.com>
2024-11-27 15:11:28 -0500

    Use SQLAlchemy instead of Flask-SQLAlchemy to define models

--- app/models/post.py
+++ app/models/post.py
@@ -1,12 +1,24 @@
 from app import db
 from datetime import datetime
+from sqlalchemy import String, Text, Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Post(db.Model):
     __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    content = db.Column(db.Text)
-    author = db.Column(db.String(255))
-    date_posted = db.Column(db.DateTime, default=datetime.now())
-    slug = db.Column(db.String(255))
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String(255))
+    content = mapped_column(Text)
+    author = mapped_column(String(255))
+    date_posted: Mapped[datetime] = mapped_column(default=datetime.now())
+    slug = mapped_column(String(255))
+
+
+# class Post(db.Model):
+#     __tablename__ = "posts"
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255))
+#     content = db.Column(db.Text)
+#     author = db.Column(db.String(255))
+#     date_posted = db.Column(db.DateTime, default=datetime.now())
+#     slug = db.Column(db.String(255))
--- app/models/user.py
+++ app/models/user.py
@@ -2,17 +2,21 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from app import db
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-    favorite_color = db.Column(db.String(120))
-    password_hash = db.Column(db.String(162))
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    date_added: Mapped[datetime] = mapped_column(default=datetime.now())
+    favorite_color: Mapped[Optional[str]] = mapped_column(String(120))
+    password_hash: Mapped[str] = mapped_column(String(162))
 
     @property
     def password(self):
@@ -27,3 +31,28 @@ class User(db.Model, UserMixin):
 
     def __repr__(self) -> str:
         return f"<Name {self.name}>"
+
+
+# class User(db.Model, UserMixin):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), nullable=False, unique=True)
+#     name = db.Column(db.String(200), nullable=False)
+#     email = db.Column(db.String(120), nullable=False, unique=True)
+#     date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+#     favorite_color = db.Column(db.String(120))
+#     password_hash = db.Column(db.String(162))
+
+#     @property
+#     def password(self):
+#         raise AttributeError("password is not accesible")
+
+#     @password.setter
+#     def password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def verify_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+#     def __repr__(self) -> str:
+#         return f"<Name {self.name}>"
