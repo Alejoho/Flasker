0fc3ec5512836dcf3af23f84d3ffe62a15ee751d
Alejoho <alejoreilly96@gmail.com>
2024-12-08 08:11:15 -0500

    Change the errorhandler from the error_routes to app_errorhandler

--- app/routes/error_routes.py
+++ app/routes/error_routes.py
@@ -3,11 +3,11 @@ from flask import Blueprint, render_template
 bp = Blueprint("error_routes", __name__)
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def page_not_found(e):
     return render_template("error/404.html"), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def page_not_found(e):
     return render_template("error/500.html"), 500
